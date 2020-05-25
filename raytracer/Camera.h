1515d43f94ddad683cf40bb57870d2190aef9b8f
x1jiang-Saki <bethouloski@gmail.com>
2020-05-25 22:55:59 +0800

    Material additions

--- raytracer/Camera.h
+++ raytracer/Camera.h
@@ -81,6 +81,12 @@ public:
 
 	virtual Ray generateRay(Vec2f point);
 	virtual float getTMin() const;
+
+	virtual void glInit(int w, int h);
+	virtual void glPlaceCamera(void);
+	virtual void dollyCamera(float dist);
+	virtual void truckCamera(float dx, float dy);
+	virtual void rotateCamera(float rx, float ry);
 };
 
 #endif // _CAMERA_H_
--- raytracer/Material.h
+++ raytracer/Material.h
@@ -2,21 +2,45 @@
 #define _MATERIAL_H_
 
 #include "_vectors.h"
+#include "_ray.h"
+#include "_hit.h"
 
-class Material {
+class Material 
+{
+protected:
+	Vec3f _diffuseColor;
 
 public:
+	Material(){}
+	Material(const Vec3f &diffuseColor) { _diffuseColor = diffuseColor; }
+	virtual ~Material() {}
 
-  // CONSTRUCTORS & DESTRUCTOR
-  Material(const Vec3f &diffuseColor) { _diffuseColor = diffuseColor; }
-  virtual ~Material() {}
+	virtual Vec3f getDiffuseColor() const { return _diffuseColor; }
 
-  // ACCESSORS
-  virtual Vec3f getDiffuseColor() const { return _diffuseColor; }
+	// computes the local interaction of light and the material
+	virtual Vec3f Shade(const Ray& ray, const Hit& hit, const Vec3f& dirToLight,const Vec3f& lightColor) const = 0;
 
-protected:
-  Vec3f _diffuseColor;
-
+	// send the appropriate OpenGL commands to specify the local shading model.
+	virtual void glSetMaterial(void) const = 0;
 };
 
+class PhongMaterial : public Material
+{
+	Vec3f _specularColor;
+	float _exponent;
+
+public:
+	PhongMaterial(const Vec3f& diffuseColor, const Vec3f& specularColor, float exponent)
+	{
+		_diffuseColor = diffuseColor;
+		_specularColor = specularColor;
+		_exponent = exponent;
+	}
+	~PhongMaterial(){}
+
+	Vec3f getSpecularColor() const { return _specularColor; }
+
+	virtual Vec3f Shade(const Ray& ray, const Hit& hit, const Vec3f& dirToLight, const Vec3f& lightColor) const;
+	virtual void glSetMaterial(void) const;
+};
 #endif //  _MATERIAL_H_
