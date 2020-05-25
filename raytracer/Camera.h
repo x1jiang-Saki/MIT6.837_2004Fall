fa3b5dea7061626bef03f77a3e9e3976a2a00e1c
x1jiang-Saki <bethouloski@gmail.com>
2020-05-25 22:39:25 +0800

    glcanvas and Camera Additions

--- raytracer/Camera.h
+++ raytracer/Camera.h
@@ -16,6 +16,12 @@ public:
 
 	virtual Ray generateRay(Vec2f ssPos) = 0;
 	virtual float getTMin() const = 0;
+
+	virtual void glInit(int w, int h) = 0;
+	virtual void glPlaceCamera(void) = 0;
+	virtual void dollyCamera(float dist) = 0;
+	virtual void truckCamera(float dx, float dy) = 0;
+	virtual void rotateCamera(float rx, float ry) = 0;
 };
 
 class OrthographicCamera : public Camera
@@ -41,6 +47,13 @@ public:
 
 	virtual Ray generateRay(Vec2f point);
 	virtual float getTMin() const;
+
+	virtual void glInit(int w, int h);
+	virtual void glPlaceCamera(void);
+	virtual void dollyCamera(float dist);
+	virtual void truckCamera(float dx, float dy);
+	virtual void rotateCamera(float rx, float ry);
+
 private:
 	float _size;
 };
--- raytracer/PerspectiveCamera.cpp
+++ raytracer/PerspectiveCamera.cpp
@@ -1,4 +1,7 @@
 #include "Camera.h"
+#include "_matrix.h"
+#include <GL/gl.h>
+#include <gl\GLU.h>
 
 Ray PerspectiveCamera::generateRay(Vec2f point)
 {
@@ -12,4 +15,109 @@ Ray PerspectiveCamera::generateRay(Vec2f point)
 float PerspectiveCamera::getTMin() const
 {
 	return 0.0f;
+}
+
+// ====================================================================
+// Create a perspective camera with the appropriate dimensions that
+// crops or stretches in the x-dimension as necessary
+// ====================================================================
+
+void PerspectiveCamera::glInit(int w, int h)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(_fov * 180.0 / 3.14159, (float)w / float(h), 0.5, 40.0);
+}
+
+// ====================================================================
+// Place a perspective camera within an OpenGL scene
+// ====================================================================
+
+void PerspectiveCamera::glPlaceCamera(void)
+{
+	gluLookAt(_center.x(), _center.y(), _center.z(),
+		_center.x() + _direction.x(), _center.y() + _direction.y(), _center.z() + _direction.z(),
+		_up.x(), _up.y(), _up.z());
+}
+
+// ====================================================================
+// dollyCamera, truckCamera, and RotateCamera
+//
+// Asumptions:
+//  - up is really up (i.e., it hasn't been changed
+//    to point to "screen up")
+//  - up and direction are normalized
+// Special considerations:
+//  - If your constructor precomputes any vectors for
+//    use in 'generateRay', you will likely to recompute those
+//    values at athe end of the these three routines
+// ====================================================================
+
+// ====================================================================
+// dollyCamera: Move camera along the direction vector
+// ====================================================================
+
+void PerspectiveCamera::dollyCamera(float dist)
+{
+	_center += _direction * dist;
+
+	// ===========================================
+	// ASSIGNMENT 3: Fix any other affected values
+	// ===========================================
+
+
+}
+
+// ====================================================================
+// truckCamera: Translate camera perpendicular to the direction vector
+// ====================================================================
+
+void PerspectiveCamera::truckCamera(float dx, float dy)
+{
+	Vec3f horizontal;
+	Vec3f::Cross3(horizontal, _direction, _up);
+	horizontal.Normalize();
+
+	Vec3f screenUp;
+	Vec3f::Cross3(screenUp, horizontal, _direction);
+
+	_center += horizontal * dx + screenUp * dy;
+
+	// ===========================================
+	// ASSIGNMENT 3: Fix any other affected values
+	// ===========================================
+
+
+}
+
+// ====================================================================
+// rotateCamera: Rotate around the up and horizontal vectors
+// ====================================================================
+
+void PerspectiveCamera::rotateCamera(float rx, float ry)
+{
+	Vec3f horizontal;
+	Vec3f::Cross3(horizontal, _direction, _up);
+	horizontal.Normalize();
+
+	// Don't let the model flip upside-down (There is a singularity
+	// at the poles when 'up' and 'direction' are aligned)
+	float tiltAngle = acos(_up.Dot3(_direction));
+	if (tiltAngle - ry > 3.13)
+		ry = tiltAngle - 3.13;
+	else if (tiltAngle - ry < 0.01)
+		ry = tiltAngle - 0.01;
+
+	Matrix rotMat = Matrix::MakeAxisRotation(_up, rx);
+	rotMat *= Matrix::MakeAxisRotation(horizontal, ry);
+
+	rotMat.Transform(_center);
+	rotMat.TransformDirection(_direction);
+	_direction.Normalize();
+
+	// ===========================================
+	// ASSIGNMENT 3: Fix any other affected values
+	// ===========================================
+
+
 }
